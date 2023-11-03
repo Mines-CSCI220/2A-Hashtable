c3c065d036424c008df0527eaf88df0060412127
Keith Hellman <khellman@mcprogramming.com>
2023-11-03 15:23:57 -0600

    analysis for 2P-

---
+++ random220.cpp
@@ -0,0 +1,55 @@
+/*
+ * random220.cpp
+ * khellman@mines.edu, see random220.h for documentation.
+ *
+*/
+
+#include <cmath>
+#include <iostream>
+#include <cassert>
+
+#include "random220.h"
+
+unsigned long seed_now()
+{
+	using std::chrono::system_clock;
+	unsigned long seed = system_clock::to_time_t( system_clock::now() ); 
+	srand48( seed );
+	std::cerr << "seed_now " << seed << std::endl; 
+	return seed;
+}
+
+bool bernoulli( const double p )
+{
+	// compilicated stuff, eh?
+	assert( 0<= p );
+	assert( p<= 1 );
+	return drand48() < p;
+}
+
+bool coinflip( ) { return bernoulli(0.5); }
+
+double uniform( const double a, const double b )
+{
+	assert( a<=b );
+	return a + drand48()*(b-a);
+}
+
+long equalikely( const long a, const long b )
+{
+	assert( a<=b );
+	return ::floor( uniform(a,b+1));
+}
+
+template<typename T>
+void fisher_yates( std::vector<T>& v, size_t count )
+{
+	// fisher yates 
+	for( size_t i=0; i<v.size()-1 && count!=0; ++i ) {
+		T hold(v[i]);
+		size_t to = equalikely(i,v.size()-1);
+		v[i] = v[to];
+		v[to] = hold;
+		--count;
+	}
+}
---
+++ random220.h
@@ -0,0 +1,51 @@
+/*
+ * random220.h
+ * CSCI220 random number untils, only slightly informed by the corpus of
+ * computer simulation.
+ * khellman@mines.edu
+ *
+ * The underlying routines use the drand48 prng, this can be seeded with
+ * srand48( long seed ) (part of the libc implementation) or this module's
+ * seed_now().
+ */
+
+#ifndef RANDOM220_H
+#define RANDOM220_H
+
+#include <vector>
+
+/***
+ * a Bernoulli random variable.  returns true with probability 0<= p <=1
+ */
+bool bernoulli( const double p );
+
+/***
+ * a Uniform random variable, returns values within (a,b) with equal probability
+ */
+double uniform( const double a, const double b );
+
+/***
+ * an Equalikely random variable, returns integer within (a,b) with equal probability
+ */
+long equalikely( const long a, const long b );
+
+/***
+ * a fair coin flip
+ */
+bool coinflip( );
+
+/***
+ * basic Fisher Yates in-place shuffle of a vector, the optional count parameter
+ * can specify a partial shuffling
+ */
+template<typename T>
+void fisher_yates( std::vector<T>& v, size_t count = 0 );
+
+
+/***
+ * seeds the generator with the computer's wall clock time, prints the seed to 
+ * cerr, and returns the value used.
+ */
+unsigned long seed_now();
+
+#endif
